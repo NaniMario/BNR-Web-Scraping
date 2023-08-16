755e78aecb2cd8300b59642aa7fc5033f1216812
Nani <113214320+NaniMario@users.noreply.github.com>
2023-08-16 14:51:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+r = requests.get('https://www.cursbnr.ro/')
+class GetRates:
+    def get_rates():
+        global currency
+        global value
+        if r.ok:
+            soup = BeautifulSoup(r.content, 'lxml')
+            currency_table = soup.find('div', class_='table-responsive')
+            currency_data = currency_table.find_all('tr')
+            currency, value = [], []
+            for item in currency_data:
+                if item.find('td'):
+                    currency.append(item.find('td').text)
+                    values = item.find_all('td')
+                    value.append(values[2].text)
+
+            data = dict(zip  (currency, value))
+            currency = data.keys()
+            value = data.values()   
+
+            
